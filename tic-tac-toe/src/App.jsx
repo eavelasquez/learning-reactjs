a82e61baf32b443d4a968d19784e420dbd501164
Esteban Velasquez <eavc.30@gmail.com>
2023-01-27 15:48:48 -0500

    feat(front): add board for tic tac toe

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
+const Square = ({ children, updateBoard, index }) => {
   return (
-    <div className="App">
-      <h1>Tic Tac Toe</h1>
+    <div className='square'>
+      {children}
     </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square key={index} index={index}>
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
   )
 }
 
