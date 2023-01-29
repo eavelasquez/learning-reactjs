20d37efd64e7e38b314258242544fe2a1471c29a
Esteban Velasquez <eavc.30@gmail.com>
2023-01-29 17:05:25 -0500

    feat(front): create twitter follow card component

--- twitter-follow-card/src/App.jsx
+++ twitter-follow-card/src/App.jsx
@@ -1,28 +1,15 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
 function App() {
   return (
-    <div className='App'>
+    <section className='App'>
       <h1>Twitter Follow Card</h1>
 
-      <article className='tw-followCard'>
-        <header className='tw-followCard-header'>
-          <img
-            className='tw-followCard-avatar'
-            src="https://unavatar.io/eavelasquez"
-            alt="eavelasquez's avatar"
-          />
-          <div className='tw-followCard-info'>
-            <strong>Esteban Velasquez</strong>
-            <span className='tw-followCard-infoUserName'>@eavelasquez</span>
-          </div>
-        </header>
-
-        <aside>
-          <button className='tw-followCard-button'>Follow</button>
-        </aside>
-      </article>
-    </div>
+      <TwitterFollowCard username='eavelasquez' name='Esteban Velasquez' />
+      <TwitterFollowCard username='nasa' name='NASA' />
+      <TwitterFollowCard username='midudev' name='Miguel Ángel Durán' />
+    </section>
   )
 }
 
