50c6d9f6047166d83b749c7db4bb82d3941d57d7
Esteban Velasquez <eavc.30@gmail.com>
2023-01-29 16:46:55 -0500

    feat(front): add html skeleton

--- twitter-follow-card/src/App.jsx
+++ twitter-follow-card/src/App.jsx
@@ -4,6 +4,20 @@ function App () {
   return (
     <div className='App'>
       <h1>Twitter Follow Card</h1>
+
+      <article>
+        <header>
+          <img src="https://unavatar.io/eavelasquez" alt="eavelasquez's avatar" />
+          <div>
+            <strong>Esteban Velasquez</strong>
+            <span>@eavelasquez</span>
+          </div>
+        </header>
+
+        <aside>
+          <button>Follow</button>
+        </aside>
+      </article>
     </div>
   )
 }
