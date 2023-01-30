6a7d3e99cd319d5ac594bac3351bb0ace1a9bc14
Esteban Velasquez <eavc.30@gmail.com>
2023-01-30 16:38:11 -0500

    feat(front): implement use reducer hook in characters

--- react-hooks/src/components/Characters.jsx
+++ react-hooks/src/components/Characters.jsx
@@ -1,10 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 
 import { getCharacters } from '../services/rick-and-morty'
+
+const initialState = {
+  favorites: []
+}
+
+const favoriteReducer = (state, action) => {
+  if (action.type === 'ADD_TO_FAVORITES') {
+    return {
+      ...state,
+      favorites: [...state.favorites, action.payload]
+    }
+  }
+
+  return state
+}
 
 export const Characters = () => {
   const [characters, setCharacters] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState)
 
   useEffect(() => {
     getCharacters().then((characters) => {
@@ -13,22 +30,50 @@ export const Characters = () => {
     })
   }, [])
 
+  const handleClick = (favorite) => {
+    if (isFavorite(favorite)) {
+      return
+    }
+    dispatch({ type: 'ADD_TO_FAVORITES', payload: favorite })
+  }
+
+  const isFavorite = (character) => {
+    return favorites.favorites.find((favorite) => favorite.id === character.id)
+  }
+
   return (
     <div className='Characters'>
+
+      <h2>Favorites</h2>
+      {
+        favorites.favorites.map((favorite) => (
+          <li key={favorite.id}>
+            {favorite.name}
+          </li>
+        ))
+      }
+
       <h2>Characters</h2>
 
       {isLoading
         ? (
           <p>Loading...</p>
-          )
+        )
         : (
-            characters.map((character) => (
-              <div className='Character' key={character.id}>
-                <img src={character.image} alt={character.name} />
-                <h3>{character.name}</h3>
-              </div>
-            ))
-          )}
+          characters.map((character) => (
+            <div className='Character-item' key={character.id}>
+              <img src={character.image} alt={character.name} />
+              <h3>{character.name}</h3>
+              <button
+                type='button'
+                onClick={() => handleClick(character)}
+                disabled={isFavorite(character)}
+              >
+                Add to favorites
+              </button>
+            </div>
+          ))
+        )}
     </div>
   )
 }
