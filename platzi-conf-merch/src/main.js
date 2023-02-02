8f5ae03490f9d6a0b517d6149b22d605b99e9255
Esteban Velasquez <eavc.30@gmail.com>
2023-02-02 15:57:06 -0500

    feat(front): setup react in platzi conf merch

--- platzi-conf-merch/src/main.js
+++
@@ -1,7 +0,0 @@
-import './index.css'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-  </div>
-`
---
+++ platzi-conf-merch/src/main.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+
+import { App } from './App'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
