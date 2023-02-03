dc12b8245ac9894defd75c33b12443861ddb79eb
Esteban Velasquez <eavc.30@gmail.com>
2023-02-03 15:27:58 -0500

    feat(front): add react paypal js dependency

--- platzi-conf-merch/src/routes/App.jsx
+++ platzi-conf-merch/src/routes/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
 import { Checkout } from '../containers/Checkout'
 import { Home } from '../containers/Home'
@@ -10,24 +11,32 @@ import { Layout } from '../components/Layout'
 import { AppContext } from '../context/AppContext'
 import { useInitialState } from '../hooks/useInitialState'
 
+const options = {
+  'client-id': '',
+  currency: 'USD',
+  intent: 'capture'
+}
+
 export const App = () => {
   const initialState = useInitialState()
 
   return (
-    <AppContext.Provider value={initialState}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/checkout' element={<Checkout />} />
-            <Route exact path='/checkout/information' element={<Information />} />
-            <Route exact path='/checkout/payment' element={<Payment />} />
-            <Route exact path='/checkout/success' element={<Success />} />
-            <Route path='*' element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AppContext.Provider>
+    <PayPalScriptProvider options={options}>
+      <AppContext.Provider value={initialState}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/checkout' element={<Checkout />} />
+              <Route exact path='/checkout/information' element={<Information />} />
+              <Route exact path='/checkout/payment' element={<Payment />} />
+              <Route exact path='/checkout/success' element={<Success />} />
+              <Route path='*' element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AppContext.Provider>
+    </PayPalScriptProvider>
   )
 }
