d9a0d03662ef457188c67cbda32a73c4b1182e82
Esteban Velasquez <eavc.30@gmail.com>
2023-02-03 10:27:14 -0500

    feat(front): update app routes file with use context

--- platzi-conf-merch/src/routes/App.jsx
+++ platzi-conf-merch/src/routes/App.jsx
@@ -6,23 +6,28 @@ import { Information } from '../containers/Information'
 import { NotFound } from '../containers/NotFound'
 import { Payment } from '../containers/Payment'
 import { Success } from '../containers/Success'
-
 import { Layout } from '../components/Layout'
+import { AppContext } from '../context/AppContext'
+import { useInitialState } from '../hooks/useInitialState'
 
 export const App = () => {
+  const initialState = useInitialState()
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/checkout' element={<Checkout />} />
-          <Route exact path='/checkout/information' element={<Information />} />
-          <Route exact path='/checkout/payment' element={<Payment />} />
-          <Route exact path='/checkout/success' element={<Success />} />
-          <Route path='*' element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/checkout' element={<Checkout />} />
+            <Route exact path='/checkout/information' element={<Information />} />
+            <Route exact path='/checkout/payment' element={<Payment />} />
+            <Route exact path='/checkout/success' element={<Success />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   )
 }
