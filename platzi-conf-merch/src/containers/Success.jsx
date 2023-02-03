fe9fae17396e12730632234acf7762cad801e3a6
Esteban Velasquez <eavc.30@gmail.com>
2023-02-03 09:14:30 -0500

    feat(front): success component layout

--- platzi-conf-merch/src/containers/Success.jsx
+++ platzi-conf-merch/src/containers/Success.jsx
@@ -2,6 +2,12 @@ import '../styles/containers/Success.css'
 
 export const Success = () => {
   return (
-    <h1>Success</h1>
+    <div className='Success'>
+      <div className='Success-content'>
+        <h2>NAME, THANKS FOR YOUR PURCHASE</h2>
+        <span>Your order will arrive in 3 business days at your address:</span>
+        <div className='Success-map'>Google Maps</div>
+      </div>
+    </div>
   )
 }
