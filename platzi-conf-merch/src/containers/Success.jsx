7950e55bb3fc954434a4b1e7f5bca1567f28da97
Esteban Velasquez <eavc.30@gmail.com>
2023-02-04 18:47:57 -0500

    feat(front): get buyer info in success container

--- platzi-conf-merch/src/containers/Success.jsx
+++ platzi-conf-merch/src/containers/Success.jsx
@@ -1,11 +1,17 @@
+import { useContext } from 'react'
+
 import '../styles/containers/Success.css'
+import { AppContext } from '../context/AppContext'
 import { Map } from '../components/Map'
 
 export const Success = () => {
+  const { state } = useContext(AppContext)
+  const { buyer } = state
+
   return (
     <div className='Success'>
       <div className='Success-content'>
-        <h2>NAME, THANKS FOR YOUR PURCHASE</h2>
+        <h2>{`${buyer.name}, thanks for your purchase`}</h2>
         <span>Your order will arrive in 3 business days at your address:</span>
         <div className='Success-map'>
           <Map />
