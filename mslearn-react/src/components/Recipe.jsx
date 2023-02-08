400c1d750351397abaa4b7d4247ac98dc8004fca
Esteban Velasquez <eavc.30@gmail.com>
2023-02-08 11:25:45 -0500

    feat(front): validate if it prepared with a use effect

--- mslearn-react/src/components/Recipe.jsx
+++ mslearn-react/src/components/Recipe.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import IngredientList from './IngredientList'
 import RecipeTitle from './RecipeTitle'
 
 export default function Recipe ({ recipeIndex, title, feedback, ingredients, onIngredientClick }) {
+  const [prepared, setPrepared] = useState(false)
+  const isPreparedText = prepared ? 'Prep work is done!' : 'Just keep chopping'
+
+  const isPrepared = () => {
+    setPrepared(ingredients.every(ingredient => ingredient.prepared))
+  }
+
+  useEffect(isPrepared, [ingredients])
+
   return (
     <>
       <RecipeTitle title={title} feedback={feedback} />
@@ -12,6 +21,7 @@ export default function Recipe ({ recipeIndex, title, feedback, ingredients, onI
         ingredients={ingredients}
         onIngredientClick={onIngredientClick}
       />
+      <p>{isPreparedText}</p>
     </>
   )
 }
