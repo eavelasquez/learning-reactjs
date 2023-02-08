796ed46b76c37541d4eba2c8dd5545063ac96a94
Esteban Velasquez <eavc.30@gmail.com>
2023-02-08 15:37:30 -0500

    feat(front): implement use ref in movie search

--- movie-search/src/App.jsx
+++ movie-search/src/App.jsx
@@ -1,13 +1,28 @@
+import { useRef } from 'react'
+
 import './App.css'
 
-function App() {
+function App () {
+  const inputRef = useRef()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const query = inputRef.current.value
+    console.log(query)
+  }
+
   return (
-    <div className="page">
+    <div className='page'>
       <header>
         <h1>Movie Search</h1>
 
-        <form className="form">
-          <input type='text' placeholder='Avengers, Star Wars, The Godfather...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            name='query'
+            placeholder='Avengers, Star Wars, The Godfather...'
+            ref={inputRef}
+          />
           <button type='submit'>Search</button>
         </form>
       </header>
