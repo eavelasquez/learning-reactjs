ba732487db6f9d3a73c4991a1b23cd6d445948e8
Esteban Velasquez <eavc.30@gmail.com>
2023-02-08 09:43:17 -0500

    feat(front): add recipe file

---
+++ mslearn-react/src/utils/recipes.js
@@ -0,0 +1,28 @@
+export const recipes = [
+  {
+    title: 'Mashed potatoes',
+    feedback: {
+      rating: 4.8,
+      reviews: 20
+    },
+    ingredients: [
+      { name: '3 potatoes, cut into 1/2" pieces', prepared: false },
+      { name: '4 Tbsp butter', prepared: false },
+      { name: '1/8 cup heavy cream', prepared: false },
+      { name: 'Salt', prepared: true },
+      { name: 'Pepper', prepared: true },
+    ]
+  },
+  {
+    title: 'Grilled cheese',
+    feedback: {
+      rating: 4.5,
+      reviews: 10
+    },
+    ingredients: [
+      { name: '2 slices bread', prepared: false },
+      { name: '2 slices cheese', prepared: false },
+      { name: 'Butter', prepared: false },
+    ]
+  }
+]
