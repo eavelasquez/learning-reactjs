f897f7e29d95ed87898641d69ff569681bb1be4a
Esteban Velasquez <eavc.30@gmail.com>
2023-02-08 11:18:08 -0500

    feat(front): implement use state hook and recipe comp

--- mslearn-react/src/App.jsx
+++ mslearn-react/src/App.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { recipes } from './utils/recipes.js'
-import RecipeTitle from './components/RecipeTitle'
-import IngredientList from './components/IngredientList'
+import { initialRecipes } from './utils/initialRecipes'
+import Recipe from './components/Recipe'
 
 function App () {
+  const [recipes, setRecipes] = useState(initialRecipes)
+
+  const handleIngredientClick = (recipeIndex, ingredientIndex) => {
+    const newRecipes = [...recipes]
+    const newIngredients = [...newRecipes[recipeIndex].ingredients]
+
+    newIngredients[ingredientIndex].prepared = !newIngredients[ingredientIndex].prepared
+    newRecipes[recipeIndex].ingredients = newIngredients
+
+    setRecipes(newRecipes)
+  }
+
   return (
     <article>
       <h1>Recipe Manager</h1>
-
       {
         recipes.map(({ title, feedback, ingredients }, index) => (
-          <React.Fragment key={index}>
-            <RecipeTitle title={title} feedback={feedback} />
-            <IngredientList ingredients={ingredients} />
-          </React.Fragment>
+          <Recipe
+            key={index}
+            recipeIndex={index}
+            title={title}
+            feedback={feedback}
+            ingredients={ingredients}
+            onIngredientClick={handleIngredientClick}
+          />
         ))
       }
     </article>
