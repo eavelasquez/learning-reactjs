e7ad711c9d61a74e79909de2c0ade567e635038b
Esteban Velasquez <eavc.30@gmail.com>
2023-02-08 12:39:08 -0500

    feat(front): add input and button to search

--- movie-search/src/App.jsx
+++ movie-search/src/App.jsx
@@ -1,6 +1,21 @@
-function App () {
+import './App.css'
+
+function App() {
   return (
-    <h1>Movie Search</h1>
+    <div className="page">
+      <header>
+        <h1>Movie Search</h1>
+
+        <form className="form">
+          <input type='text' placeholder='Avengers, Star Wars, The Godfather...' />
+          <button type='submit'>Search</button>
+        </form>
+      </header>
+
+      <main>
+        ...results
+      </main>
+    </div>
   )
 }
 
