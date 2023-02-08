c3e0c36a475abd5d2795ba42283757d8702d10ef
Esteban Velasquez <eavc.30@gmail.com>
2023-02-08 09:27:59 -0500

    feat(front): add recipe title component

--- mslearn-react/src/App.jsx
+++ mslearn-react/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import RecipeTitle from './components/RecipeTitle'
+
 function App () {
   return (
     <article>
       <h1>Recipe Manager</h1>
+      <RecipeTitle />
     </article>
   )
 }
---
+++ mslearn-react/src/components/RecipeTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RecipeTitle () {
+  const title = 'Mashed Potatoes'
+
+  return (
+    <h1>{title}</h1>
+  )
+}
