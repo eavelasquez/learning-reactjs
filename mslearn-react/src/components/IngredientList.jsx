9ea987b88c3ce9ffb76bcc9569af3c847b2752cf
Esteban Velasquez <eavc.30@gmail.com>
2023-02-08 11:16:35 -0500

    feat(front): update ingredient list component

--- mslearn-react/src/components/IngredientList.jsx
+++ mslearn-react/src/components/IngredientList.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 import '../styles/IngredientList.css'
 
-export default function IngredientList ({ ingredients }) {
+export default function IngredientList ({ recipeKey, ingredients, onIngredientClick }) {
   const ingredientListItems = ingredients.map((ingredient, index) => {
     const className = ingredient.prepared ? 'prepared' : ''
 
+    const handleClick = () => onIngredientClick(recipeKey, index)
+
     return (
-      <li key={index} className={className}>
+      <li key={index} className={className} onClick={handleClick}>
         {ingredient.name}
       </li>
     )
   })
 
-  return <ul>{ingredientListItems}</ul>
+  return (
+    <ul className='ingredient-list'>
+      {ingredientListItems}
+    </ul>
+  )
 }
