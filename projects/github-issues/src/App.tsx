54069acd5805b103f621b3ebbc6512be7af93e6b
Esteban Velasquez <eavc.30@gmail.com>
2023-02-12 16:44:30 -0500

    feat(front): update app component in github issues

--- projects/github-issues/src/App.tsx
+++ projects/github-issues/src/App.tsx
@@ -1,7 +1,11 @@
-export const App = () => {
+import { FC } from 'react'
+import { Outlet } from 'react-router-dom'
+
+export const App: FC = () => {
   return (
-    <div className='App'>
-      <h1>GitHub Issues</h1>
+    <div className='container mt-3'>
+      <h1>GitHub Issues <small className='text-muted'>issue tracker</small></h1>
+      <Outlet />
     </div>
   )
 }
