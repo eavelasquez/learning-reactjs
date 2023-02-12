85616041c87e5b7051a9640fce913a02e01faa35
Esteban Velasquez <eavc.30@gmail.com>
2023-02-12 18:51:07 -0500

    feat(front): install and setup react query in github

--- projects/github-issues/src/main.tsx
+++ projects/github-issues/src/main.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { router } from './router'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
