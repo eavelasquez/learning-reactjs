8d07978a1ace372e23b193d65c1eb6a93a62a276
Esteban Velasquez <eavc.30@gmail.com>
2023-02-12 16:30:29 -0500

    feat(front): update imports in views

--- projects/github-issues/src/issues/views/IssueDetailView.tsx
+++ projects/github-issues/src/issues/views/IssueDetailView.tsx
@@ -1,7 +1,10 @@
+import { IssueItem } from "../../components"
+
 export const IssueDetailView = () => {
   return (
     <div>
       <h1>Issue Detail View</h1>
+      <IssueItem />
     </div>
   )
 }
--- projects/github-issues/src/issues/views/IssueListView.tsx
+++ projects/github-issues/src/issues/views/IssueListView.tsx
@@ -1,7 +1,10 @@
+import { IssueList } from "../../components"
+
 export const IssueListView = () => {
   return (
     <div>
       <h1>Issues</h1>
+      <IssueList />
     </div>
   )
 }
