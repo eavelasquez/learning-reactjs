c9aa966c128cb11be6dacd3cd8f412803e7312d6
Esteban Velasquez <eavc.30@gmail.com>
2023-02-12 16:30:43 -0500

    feat(front): add router in github issues

---
+++ projects/github-issues/src/router/index.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+
+import { App } from '../App';
+import { IssueDetailView, IssueListView, NotFoundView } from '../issues/views';
+
+export const router = createBrowserRouter([
+  {
+    path: '/issues',
+    element: <App />,
+    children: [
+      { path: '/list', element: <IssueListView /> },
+      { path: '/issue/:issueId', element: <IssueDetailView /> },
+      { path: '*', element: <Navigate to='/' /> }
+    ],
+  },
+  { path: '/', element: <Navigate to='/issues/list' /> },
+  { path: '*', element: <NotFoundView /> }
+])
