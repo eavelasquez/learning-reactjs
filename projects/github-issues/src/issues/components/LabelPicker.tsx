5698eeb2b05af1629550688ae96ffbae6ca3f64f
Esteban Velasquez <eavc.30@gmail.com>
2023-02-12 21:01:39 -0500

    feat(front): update label picker component

--- projects/github-issues/src/issues/components/LabelPicker.tsx
+++ projects/github-issues/src/issues/components/LabelPicker.tsx
@@ -1,12 +1,25 @@
+import { useLabels } from '../hooks/useLabels'
+
 export const LabelPicker = () => {
+  const labelsQuery = useLabels()
+
   return (
     <div>
-      <span
-        className='badge rounded-pill m-1 label-picker'
-        style={{ border: '1px solid #ffccd3', color: '#ffccd3' }}
-      >
-        Primary
-      </span>
+      {labelsQuery.isLoading && <div>Loading...</div>}
+      {labelsQuery.isError && <div>Error: {`${labelsQuery.error}`}</div>}
+      {labelsQuery.isSuccess && (
+        <>
+          {labelsQuery.data?.map(({ id, name, color }) => (
+            <span
+              key={id}
+              className='badge rounded-pill m-1 label-picker'
+              style={{ border: `1px solid #${color}`, color: `#${color}` }}
+            >
+              {name}
+            </span>
+          ))}
+        </>
+      )}
     </div>
   )
 }
