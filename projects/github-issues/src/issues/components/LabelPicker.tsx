2e50bc9bccd13ea9815c5ae1b4794278d3d6f26c
Esteban Velasquez <eavc.30@gmail.com>
2023-02-13 11:01:36 -0500

    feat(front): add selected labels logic

--- projects/github-issues/src/issues/components/LabelPicker.tsx
+++ projects/github-issues/src/issues/components/LabelPicker.tsx
@@ -1,8 +1,19 @@
+import { FC } from 'react'
 import { LoadingIcon } from '../../shared/components/LoadingIcon'
 import { useLabels } from '../hooks/useLabels'
 
-export const LabelPicker = () => {
+interface LabelPickerProps {
+  selectedLabels: string[]
+  onLabelSelection: (label: string) => void
+}
+
+export const LabelPicker: FC<LabelPickerProps> = ({
+  selectedLabels,
+  onLabelSelection
+}) => {
   const labelsQuery = useLabels()
+
+  const isLabelSelected = (label: string) => selectedLabels.includes(label)
 
   return (
     <div>
@@ -13,8 +24,9 @@ export const LabelPicker = () => {
           {labelsQuery.data?.map(({ id, name, color }) => (
             <span
               key={id}
-              className='badge rounded-pill m-1 label-picker'
+              className={`badge rounded-pill m-1 label-picker ${isLabelSelected(name) ? 'label-active' : ''}`}
               style={{ border: `1px solid #${color}`, color: `#${color}` }}
+              onClick={() => onLabelSelection(name)}
             >
               {name}
             </span>
--- projects/github-issues/src/issues/views/IssueListView.tsx
+++ projects/github-issues/src/issues/views/IssueListView.tsx
@@ -1,6 +1,17 @@
+import { useState } from 'react'
 import { IssueList, LabelPicker } from '../components'
 
 export const IssueListView = () => {
+  const [selectedLabels, setSelectedLabels] = useState<string[]>([])
+
+  const handleLabelSelection = (label: string) => {
+    if (selectedLabels.includes(label)) {
+      setSelectedLabels(selectedLabels.filter(l => l !== label))
+    } else {
+      setSelectedLabels([...selectedLabels, label])
+    }
+  }
+
   return (
     <div className='row mt-5'>
       <div className='col-8'>
@@ -8,7 +19,10 @@ export const IssueListView = () => {
       </div>
 
       <div className='col-4'>
-        <LabelPicker />
+        <LabelPicker
+          selectedLabels={selectedLabels}
+          onLabelSelection={handleLabelSelection}
+        />
       </div>
     </div>
   )
