61d750f7141222ad18401c9c2041f97b1c168b9d
Esteban Velasquez <eavc.30@gmail.com>
2023-02-13 16:19:48 -0500

    feat(front): add get issue function

--- projects/github-issues/src/api/github.ts
+++ projects/github-issues/src/api/github.ts
@@ -21,3 +21,8 @@ export const getIssues = async (): Promise<Issue[]> => {
   const { data } = await githubApi.get<Issue[]>('/issues')
   return data
 }
+
+export const getIssue = async (id: number): Promise<Issue> => {
+  const { data } = await githubApi.get<Issue>(`/issues/${id}`)
+  return data
+}
