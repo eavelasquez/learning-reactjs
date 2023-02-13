120bbe909b3d5859c979b581b158cd7df5030082
Esteban Velasquez <eavc.30@gmail.com>
2023-02-13 10:48:15 -0500

    feat(front): add placeholder data and initial data props

--- projects/github-issues/src/issues/hooks/useLabels.tsx
+++ projects/github-issues/src/issues/hooks/useLabels.tsx
@@ -7,8 +7,29 @@ export const useLabels = () => {
     ['labels'],
     getLabels,
     {
-      refetchOnWindowFocus: false,
-      staleTime: 1000 * 60 * 60 // 1 hour
+      refetchOnWindowFocus: false, // don't refetch when window is focused
+      staleTime: 1000 * 60 * 60, // 1 hour
+      // initialData: [], // initial data to show
+      placeholderData: [
+        {
+          id: 725156255,
+          node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+          name: "good first issue (taken)",
+          color: "b60205",
+          default: false,
+          description: null
+        },
+        {
+          id: 717031390,
+          node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+          name: "good first issue",
+          color: "6ce26a",
+          default: true,
+          description: null
+        }
+      ] // while loading, show this data
     }
   )
 
