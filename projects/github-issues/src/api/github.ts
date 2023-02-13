b4382f8297266b7c27188f8050b6f01900251b4d
Esteban Velasquez <eavc.30@gmail.com>
2023-02-13 15:02:04 -0500

    feat(front): add github access token

--- projects/github-issues/src/api/github.ts
+++ projects/github-issues/src/api/github.ts
@@ -1,11 +1,14 @@
-import axios from 'axios';
+import axios from 'axios'
 
-import { Label } from '../issues/interfaces/label';
+import { Label } from '../issues/interfaces/label'
+
+const { VITE_GITHUB_ACCESS_TOKEN } = import.meta.env
 
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com',
   headers: {
-    Accept: 'application/vnd.github.v3+json'
+    Accept: 'application/vnd.github.v3+json',
+    Authorization: `Bearer ${VITE_GITHUB_ACCESS_TOKEN}`
   }
 })
 
