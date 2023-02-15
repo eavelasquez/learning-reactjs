c0ca1683ac0125c12762a786096201dc2548fd78
Esteban Velasquez <eavc.30@gmail.com>
2023-02-15 15:55:39 -0500

    feat(front): add products component in shopping cart

--- projects/shopping-cart/src/App.jsx
+++ projects/shopping-cart/src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import { Products } from './components/Products'
+import { products } from './mocks/products'
 
 function App () {
   return (
     <div className='App'>
       <h1>Shopping Cart 🛒</h1>
+      <Products products={products} />
     </div>
   )
 }
---
+++ projects/shopping-cart/src/components/Products.jsx
@@ -0,0 +1,25 @@
+import './Products.css'
+
+import { AddToCartIcon } from './Icons'
+
+export function Products({ products }) {
+  return (
+    <main className="products">
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.thumbnail} alt={product.title} />
+            <div>
+              <strong>{product.title}</strong> - ${product.price}
+            </div>
+            <div>
+              <button type='button'>
+                <AddToCartIcon />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
