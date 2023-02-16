0e16a9d6686f2d00fbe4aaa30dd873068cbf7202
Esteban Velasquez <eavc.30@gmail.com>
2023-02-16 09:12:01 -0500

    feat(front): add initial filters logic

--- projects/shopping-cart/src/App.jsx
+++ projects/shopping-cart/src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
+
 import { Products } from './components/Products'
-import { products } from './mocks/products'
+import { products as initialState } from './mocks/products'
 
 function App () {
+  const [products] = useState(initialState)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minimumPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minimumPrice &&
+        (filters.category === 'all' || product.category === filters.category)
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
   return (
     <div className='App'>
       <h1>Shopping Cart 🛒</h1>
-      <Products products={products} />
+      <Products products={filteredProducts} />
     </div>
   )
 }
