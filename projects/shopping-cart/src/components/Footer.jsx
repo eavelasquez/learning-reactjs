ca81ec674e0abfdc67d52948576afc102793c527
Esteban Velasquez <eavc.30@gmail.com>
2023-02-16 16:58:23 -0500

    feat(front): update footer component

--- projects/shopping-cart/src/components/Footer.jsx
+++ projects/shopping-cart/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/Footer.css'
 export function Footer ({ filters }) {
   return (
     <footer className='footer'>
-      <h4>Shopping Cart React ⚛️ － <span>@eavelasquez</span></h4>
+      <h4>Shopping Cart React ⚛️ －<span>@eavelasquez</span></h4>
       <pre>{JSON.stringify(filters, null, 2)}</pre>
     </footer>
   )
