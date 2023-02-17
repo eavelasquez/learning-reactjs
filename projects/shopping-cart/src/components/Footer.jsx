841cc3e402f4e4912913116530d3e01b2880675c
Esteban Velasquez <eavc.30@gmail.com>
2023-02-17 14:28:26 -0500

    feat(front): remove pre tag

--- projects/shopping-cart/src/components/Footer.jsx
+++ projects/shopping-cart/src/components/Footer.jsx
@@ -1,13 +1,9 @@
 import '../styles/Footer.css'
-import { useFilters } from '../hooks'
 
 export function Footer () {
-  const { filters } = useFilters()
-
   return (
     <footer className='footer'>
       <h4>Shopping Cart React ⚛️ －<span>@eavelasquez</span></h4>
-      <pre>{JSON.stringify(filters, null, 2)}</pre>
     </footer>
   )
 }
