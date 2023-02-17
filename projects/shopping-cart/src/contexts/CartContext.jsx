f96ac32251ab207146bb0414520f7f4f3129d95d
Esteban Velasquez <eavc.30@gmail.com>
2023-02-17 13:30:35 -0500

    feat(front): create use cart reducer hook

--- projects/shopping-cart/src/contexts/CartContext.jsx
+++ projects/shopping-cart/src/contexts/CartContext.jsx
@@ -1,36 +1,21 @@
-import { createContext, useReducer, useState } from 'react'
-import { cartReducer } from '../reducers/cartReducer'
+import { createContext } from 'react'
 
-const CART_ACTION_TYPES = {
-  ADD_TO_CART: 'ADD_TO_CART',
-  REMOVE_FROM_CART: 'REMOVE_FROM_CART',
-  CLEAR_CART: 'CLEAR_CART'
-}
+import { useCartReducer } from '../hooks/useCartReducer'
 
 export const CartContext = createContext()
 
 export const CartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(cartReducer, cartInitialState)
-
-  const addToCart = (product) => {
-    dispatch({ type: CART_ACTION_TYPES.ADD_TO_CART, payload: product })
-  }
-
-  const removeFromCart = (id) => {
-    dispatch({ type: CART_ACTION_TYPES.REMOVE_FROM_CART, payload: { id } })
-  }
-
-  const clearCart = () => {
-    dispatch({ type: CART_ACTION_TYPES.CLEAR_CART })
-  }
-
-  const isInCart = (id) => {
-    return state.some((item) => item.id === id)
-  }
+  const {
+    cart,
+    addToCart,
+    removeFromCart,
+    clearCart,
+    isInCart
+  } = useCartReducer()
 
   return (
     <CartContext.Provider value={{
-      cart: state,
+      cart,
       addToCart,
       removeFromCart,
       clearCart,
