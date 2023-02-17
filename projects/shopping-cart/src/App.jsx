5c22e8c49d0b4e27716bd8eee45ee50dd9fd8d19
Esteban Velasquez <eavc.30@gmail.com>
2023-02-16 19:59:53 -0500

    feat(front): add use context in app component

--- projects/shopping-cart/src/App.jsx
+++ projects/shopping-cart/src/App.jsx
@@ -1,4 +1,5 @@
 import { Cart, Footer, Header, Products } from './components'
+import { CartContext } from './contexts/CartContext'
 import { IS_DEVELOPMENT } from './config'
 import { products as initialProducts } from './mocks/products'
 import { useFilters } from './hooks'
@@ -9,12 +10,12 @@ function App () {
   const filteredProducts = filterProducts(initialProducts)
 
   return (
-    <>
+    <CartContext.Provider>
       <Header />
       <Cart />
       <Products products={filteredProducts} />
       {IS_DEVELOPMENT && <Footer />}
-    </>
+    </CartContext.Provider>
   )
 }
 
