f0e08a82297f51de9858a47b8a3fa9a9e6b146de
Esteban Velasquez <eavc.30@gmail.com>
2023-02-17 13:51:39 -0500

    feat(front): use storage functions in cart reducer

--- projects/shopping-cart/src/reducers/cartReducer.js
+++ projects/shopping-cart/src/reducers/cartReducer.js
@@ -1,32 +1,45 @@
+import {
+  getCartFromStorage,
+  removeCartFromStorage,
+  updateCartToStorage
+} from '../utils/storage'
+
 export const CART_ACTION_TYPES = {
   ADD_TO_CART: 'ADD_TO_CART',
   REMOVE_FROM_CART: 'REMOVE_FROM_CART',
   CLEAR_CART: 'CLEAR_CART'
 }
 
-export const cartInitialState = []
+export const cartInitialState = getCartFromStorage()
 
 export const cartReducer = (state, action) => {
   const { type, payload } = action
 
+  let newState = []
   if (type === CART_ACTION_TYPES.ADD_TO_CART) {
-    const { id } = payload
     // Check if product is already in cart
-    const productInCart = state.findIndex((item) => item.id === id)
+    const productInCart = state.findIndex((item) => item.id === payload.id)
 
     // If product is already in cart, increase quantity by 1
+    // Otherwise, add product to cart
     if (productInCart !== -1) {
       const newCart = structuredClone(state)
       newCart[productInCart].quantity += 1
-      return newCart
+      newState = newCart
+    } else {
+      newState = [...state, { ...payload, quantity: 1 }]
     }
-    // Otherwise, add product to cart
-    return [...state, { ...payload, quantity: 1 }]
+
+    updateCartToStorage(newState)
+    return newState
   } else if (type === CART_ACTION_TYPES.REMOVE_FROM_CART) {
-    const { id } = payload
     // Filter out product with matching id
-    return state.filter((item) => item.id !== id)
+    newState = state.filter((item) => item.id !== payload.id)
+
+    updateCartToStorage(newState)
+    return newState
   } else if (type === CART_ACTION_TYPES.CLEAR_CART) {
+    removeCartFromStorage()
     return cartInitialState
   } else {
     throw Error(`Unknown action type: ${type}`)
