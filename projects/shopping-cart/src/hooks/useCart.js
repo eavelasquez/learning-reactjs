96596b1f42fdf9e65461625e100b80636f3d8197
Esteban Velasquez <eavc.30@gmail.com>
2023-02-16 20:36:56 -0500

    refactor(front): desestructuring use cart context

--- projects/shopping-cart/src/hooks/useCart.js
+++ projects/shopping-cart/src/hooks/useCart.js
@@ -2,12 +2,14 @@ import { useContext } from 'react'
 
 import { CartContext } from '../contexts/CartContext'
 
-export function useCart () {
+export function useCart() {
   const context = useContext(CartContext)
 
   if (!context) {
     throw new Error('useCart must be used within a CartProvider')
   }
 
-  return context
+  const { cart, addToCart, removeFromCart, clearCart, isInCart } = context
+
+  return { cart, addToCart, removeFromCart, clearCart, isInCart }
 }
