600729b2daa18643e3305e5b29a76ef9358c8c1d
Esteban Velasquez <eavc.30@gmail.com>
2023-02-17 13:31:55 -0500

    feat(front): move cart action types const

--- projects/shopping-cart/src/reducers/cartReducer.js
+++ projects/shopping-cart/src/reducers/cartReducer.js
@@ -1,9 +1,15 @@
+export const CART_ACTION_TYPES = {
+  ADD_TO_CART: 'ADD_TO_CART',
+  REMOVE_FROM_CART: 'REMOVE_FROM_CART',
+  CLEAR_CART: 'CLEAR_CART'
+}
+
 export const cartInitialState = []
 
 export const cartReducer = (state, action) => {
   const { type, payload } = action
 
-  if (type === 'ADD_TO_CART') {
+  if (type === CART_ACTION_TYPES.ADD_TO_CART) {
     const { id } = payload
     // Check if product is already in cart
     const productInCart = state.findIndex((item) => item.id === id)
@@ -16,11 +22,11 @@ export const cartReducer = (state, action) => {
     }
     // Otherwise, add product to cart
     return [...state, { ...payload, quantity: 1 }]
-  } else if (type === 'REMOVE_FROM_CART') {
+  } else if (type === CART_ACTION_TYPES.REMOVE_FROM_CART) {
     const { id } = payload
     // Filter out product with matching id
     return state.filter((item) => item.id !== id)
-  } else if (type === 'CLEAR_CART') {
+  } else if (type === CART_ACTION_TYPES.CLEAR_CART) {
     return cartInitialState
   } else {
     throw Error(`Unknown action type: ${type}`)
