b8c9e5ff102ccebd80833e700f1a0c472ed1495f
Esteban Velasquez <eavc.30@gmail.com>
2023-02-20 16:37:55 -0500

    feat(front): update app component in media recorder

--- projects/media-recorder/src/App.jsx
+++ projects/media-recorder/src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react'
+
+import { AudioRecorder, VideoRecorder } from './components'
+
 function App () {
+  const [recordOption, setRecordOption] = useState('audio')
+
+  const toggleRecordOption = (option) => () => setRecordOption(option)
+
   return (
-    <div className='App'>
+    <>
       <h1>Media Recorder</h1>
-    </div>
+      <div className='button-flex'>
+        <button type='button' onClick={toggleRecordOption('audio')}>
+          Audio Recorder
+        </button>
+        <button type='button' onClick={toggleRecordOption('video')}>
+          Video Recorder
+        </button>
+
+        {recordOption === 'audio' ? <AudioRecorder /> : <VideoRecorder />}
+      </div>
+    </>
   )
 }
 
