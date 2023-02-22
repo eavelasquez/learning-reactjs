014cf888dceff82d9714eb9161492ebd2c79c1ac
Esteban Velasquez <eavc.30@gmail.com>
2023-02-22 12:38:25 -0500

    feat: add homepage and aboutpage components

--- projects/react-router/src/App.jsx
+++ projects/react-router/src/App.jsx
@@ -1,8 +1,36 @@
-function App () {
+import { useState } from 'react'
+
+function HomePage () {
   return (
-    <div className='App'>
-      <h1>React Router</h1>
-    </div>
+    <>
+      <h1>Home Page</h1>
+      <p>This is the home page</p>
+      <a href='/about'>Go to about page</a>
+    </>
+  )
+}
+
+function AboutPage () {
+  return (
+    <>
+      <h1>About Page</h1>
+      <div>
+        <img src='https://placekitten.com/200/300' alt='kitten' />
+        <p>This is the about page</p>
+      </div>
+      <a href='/'>Go back home</a>
+    </>
+  )
+}
+
+function App () {
+  const [currentPage, setCurrentPage] = useState(window.location.pathname)
+
+  return (
+    <main>
+      {currentPage === '/' && <HomePage />}
+      {currentPage === '/about' && <AboutPage />}
+    </main>
   )
 }
 
