47e1f965935a4483d33f9a167a21fbd1dc45f859
Esteban Velasquez <eavc.30@gmail.com>
2023-03-03 22:07:33 -0500

    feat(front): rename todos to todo-list component

--- projects/todomvc/src/components/todo-list.tsx
+++ projects/todomvc/src/components/todo-list.tsx
@@ -3,13 +3,13 @@ import { type FunctionComponent } from 'preact'
 import { TodoItem } from './todo-item'
 import { type ListOfTodos, type TodoId, type TodoIdCompleted } from '../types'
 
-interface TodosProps {
+interface TodoListProps {
   todos: ListOfTodos
   onRemoveTodo: ({ id }: TodoId) => void
   onToggleTodo: ({ id, completed }: TodoIdCompleted) => void
 }
 
-export const Todos: FunctionComponent<TodosProps> = ({ todos, onRemoveTodo, onToggleTodo }) => {
+export const TodoList: FunctionComponent<TodoListProps> = ({ todos, onRemoveTodo, onToggleTodo }) => {
   return (
     <ul className='todo-list'>
       {todos.map((todo) => (
