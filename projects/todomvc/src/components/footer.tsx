cb80fb59fcd743aa8929eb131ce812d0de654285
Esteban Velasquez <eavc.30@gmail.com>
2023-03-03 21:17:29 -0500

    feat(front): add new footer component to todos app

---
+++ projects/todomvc/src/components/footer.tsx
@@ -0,0 +1,26 @@
+import { type FunctionComponent } from 'preact'
+
+import { Filters } from './filters'
+import { type TodoFilter } from '../types'
+
+export interface FooterProps {
+  activeCount: number
+  completedCount: number
+  filterSelected: TodoFilter
+  onClearCompleted: () => void
+  onFilterSelected: (filter: TodoFilter) => void
+}
+
+export const Footer: FunctionComponent<FooterProps> = (
+  { activeCount, completedCount, filterSelected, onClearCompleted, onFilterSelected }
+) => {
+  return (
+    <footer className='footer'>
+      <span className='todo-count'>
+        <strong>{activeCount}</strong> {activeCount === 1 ? 'item' : 'items'} left
+      </span>
+      <Filters filterSelected={filterSelected} onFilterSelected={onFilterSelected} />
+      <button className='clear-completed'>Clear completed</button>
+    </footer>
+  )
+}
