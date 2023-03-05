e119864284e10b73c9b9f98a85955a03ac81a6ad
Esteban Velasquez <eavc.30@gmail.com>
2023-03-05 11:03:20 -0500

    refactor: update not found component

--- projects/react-router/src/pages/NotFound.jsx
+++ projects/react-router/src/pages/NotFound.jsx
@@ -1,9 +1,14 @@
+import { Link } from '../components'
+
 export default function NotFoundPage () {
   return (
     <>
       <h1>Not Found - 404</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <img src='https://midu.dev/images/this-is-fine-404.gif' alt='404 gif' />
+      <div>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <img src='https://midu.dev/images/this-is-fine-404.gif' alt='404 gif' />
+      </div>
+      <Link to='/'>Go to home page</Link>
     </>
   )
 }
