23044a3df271930b27d2711ac85e4a69e6a85e82
Esteban Velasquez <eavc.30@gmail.com>
2023-03-05 09:58:48 -0500

    feat: complete basic spa navigation in react router

--- projects/react-router/src/App.jsx
+++ projects/react-router/src/App.jsx
@@ -1,10 +1,12 @@
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { EVENTS } from './constants'
+import { useEffect, useState } from 'react'
+
+import { EVENTS } from './utils/consts'
+
+const { POPSTATE, PUSHSTATE } = EVENTS
 
 function navigate (href) {
   window.history.pushState({}, '', href)
-  const event = new Event(EVENTS.PUSHSTATE)
+  const event = new Event(PUSHSTATE)
   window.dispatchEvent(event)
 }
 
@@ -12,8 +14,8 @@ function HomePage () {
   return (
     <>
       <h1>Home Page</h1>
-      <p>This is the home page</p>
-      <a href='/about'>Go to about page</a>
+      <p>This is the home page of the React Router</p>
+      <button onClick={() => navigate('/about')}>Go to about page</button>
     </>
   )
 }
@@ -23,8 +25,8 @@ function AboutPage () {
     <>
       <h1>About Page</h1>
       <div>
-        <img src='https://placekitten.com/200/300' alt='kitten' />
-        <p>This is the about page</p>
+        <img src='https://pbs.twimg.com/profile_images/1580945539700506627/lb7Kxuu4_400x400.jpg' alt='eavelasquez73 photo' />
+        <p>Hi! My name is Alexander and I'm creating this clone of React Router</p>
       </div>
       <button onClick={() => navigate('/')}>Go to home page</button>
     </>
@@ -32,26 +34,26 @@ function AboutPage () {
 }
 
 function App () {
-  const [currentPage, setCurrentPage] = useState(window.location.pathname)
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
   useEffect(() => {
     const onLocationChange = () => {
-      setCurrentPage(window.location.pathname)
+      setCurrentPath(window.location.pathname)
     }
 
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
+    window.addEventListener(PUSHSTATE, onLocationChange)
+    window.addEventListener(POPSTATE, onLocationChange)
 
     return () => {
-      window.removeEventListener(NAVIGATION_PUSH_EVENT, onLocationChange)
-      window.removeEventListener(NAVIGATION_POP_EVENT, onLocationChange)
+      window.removeEventListener(PUSHSTATE, onLocationChange)
+      window.removeEventListener(POPSTATE, onLocationChange)
     }
   }, [])
 
   return (
     <main>
-      {currentPage === '/' && <HomePage />}
-      {currentPage === '/about' && <AboutPage />}
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
     </main>
   )
 }
