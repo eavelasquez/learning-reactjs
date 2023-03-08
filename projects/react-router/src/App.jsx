4c55550a9023b5dbc6c4269e46170c444400511d
Esteban Velasquez <eavc.30@gmail.com>
2023-03-07 20:03:48 -0500

    feat: implement lazy load in react router

--- projects/react-router/src/App.jsx
+++ projects/react-router/src/App.jsx
@@ -1,5 +1,14 @@
-import { AboutPage, HomePage, NotFoundPage, SearchPage } from './pages'
+import { lazy, Suspense } from 'react'
+
+// static imports
+// import { AboutPage, HomePage, NotFoundPage, SearchPage } from './pages'
 import { Route, Router } from './components'
+
+// dynamic imports
+const AboutPage = lazy(() => import('./pages/About.jsx'))
+const HomePage = lazy(() => import('./pages/Home.jsx'))
+const NotFoundPage = lazy(() => import('./pages/NotFound.jsx'))
+const SearchPage = lazy(() => import('./pages/Search.jsx'))
 
 const routes = [
   {
@@ -11,10 +20,12 @@ const routes = [
 function App () {
   return (
     <main>
-      <Router routes={routes} defaultComponent={NotFoundPage}>
-        <Route path='/' Component={HomePage} />
-        <Route path='/about' Component={AboutPage} />
-      </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router routes={routes} defaultComponent={NotFoundPage}>
+          <Route path='/' Component={HomePage} />
+          <Route path='/about' Component={AboutPage} />
+        </Router>
+      </Suspense>
     </main>
   )
 }
