c0ad274eb10a3fe9526fe2a011f60c9ed42ce25d
Esteban Velasquez <eavc.30@gmail.com>
2023-03-10 09:13:55 -0500

    feat: add new func perform unit of work and workLoop

--- projects/pulse/src/pulse.js
+++ projects/pulse/src/pulse.js
@@ -47,6 +47,70 @@ function render (element, container) {
   }
 }
 
+let nextUnitOfWork = null
+
+function performUnitOfWork (fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  // create new fibers
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const { type, props } = elements[index]
+
+    const newFiber = {
+      type,
+      props,
+      parent: fiber,
+      dom: null
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index += 1
+  }
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+
+function requestIdleCallback (workLoop) { }
+
+function workLoop (deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
 export default {
   createElement,
   createTextElement,
