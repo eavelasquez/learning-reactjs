f697dfa049175d5c5a6a84f3aa4d9470906c70d5
Esteban Velasquez <eavc.30@gmail.com>
2023-03-10 11:20:42 -0500

    feat: update sample pulse

--- projects/pulse/src/main.js
+++ projects/pulse/src/main.js
@@ -1,24 +1,27 @@
 import Pulse from './pulse.js'
 
-const element = Pulse.createElement(
-  'div',
-  null,
-  Pulse.createElement(
-    'h1',
-    {
-      style: 'color: tomato; text-decoration: underline'
-    },
-    Pulse.createTextElement('Hello, World')
-  ),
-  Pulse.createElement(
-    'h2',
-    {
-      style: 'text-align: right'
-    },
-    Pulse.createTextElement('from Pulse.js!')
+function App (props) {
+  return Pulse.createElement(
+    'div',
+    null,
+    Pulse.createElement(
+      'h1',
+      {
+        style: 'color: tomato; text-decoration: underline'
+      },
+      `Hello, ${props.name}`
+    ),
+    Pulse.createElement(
+      'h2',
+      {
+        style: 'text-align: right'
+      },
+      'from Pulse.js!'
+    )
   )
-)
+}
 
+const element = Pulse.createElement(App, { name: 'World' })
 const container = document.getElementById('root')
 
 Pulse.render(element, container)
