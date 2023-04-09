e4375a83ee64c5b4498a5a62d7819c961aa14fe7
eavelasquez <eavc.30@gmail.com>
2023-04-09 12:25:43 -0500

    feat: setup taildwind in crud react redux toolkit

---
+++ projects/crud-react-redux-toolkit/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+export const content = [
+	"./index.html",
+	"./src/**/*.{js,ts,jsx,tsx}",
+
+	// Path to the tremor module
+	"./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+	extend: {},
+};
+export const plugins = [];
