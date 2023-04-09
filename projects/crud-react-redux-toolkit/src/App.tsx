e4b23f9868123de417b7a5725e4df15faa34d6e5
eavelasquez <eavc.30@gmail.com>
2023-04-09 15:43:29 -0500

    feat: add grid component in app

--- projects/crud-react-redux-toolkit/src/App.tsx
+++ projects/crud-react-redux-toolkit/src/App.tsx
@@ -1,10 +1,18 @@
+import { Col, Grid } from "@tremor/react";
+
 import "./App.css";
+import CreateNewUser from "./components/CreateNewUser";
 import ListOfUsers from "./components/ListOfUsers";
 
 function App() {
 	return (
 		<>
-			<ListOfUsers />
+			<Grid numCols={1} numColsSm={2} numColsLg={3} className="gap-2">
+				<Col numColSpan={1} numColSpanLg={2}>
+					<ListOfUsers />
+				</Col>
+				<CreateNewUser />
+			</Grid>
 		</>
 	);
 }
