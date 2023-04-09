615d9dc20c7839e1ba56a0e041a57997c72e95fd
eavelasquez <eavc.30@gmail.com>
2023-04-09 13:54:26 -0500

    feat: create a custom hook to use app selector and dispatch

---
+++ projects/crud-react-redux-toolkit/src/hooks/useStore.ts
@@ -0,0 +1,10 @@
+import {
+	useDispatch,
+	useSelector,
+	type TypedUseSelectorHook,
+} from "react-redux";
+
+import { AppDispatch, RootState } from "../store";
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
