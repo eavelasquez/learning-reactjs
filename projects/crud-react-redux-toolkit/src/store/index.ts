dfa51fbeb0ae6dadb6c263c2b2ccf5c21576bcb2
eavelasquez <eavc.30@gmail.com>
2023-04-09 19:57:55 -0500

    feat: add rollback user reducer function

--- projects/crud-react-redux-toolkit/src/store/index.ts
+++ projects/crud-react-redux-toolkit/src/store/index.ts
@@ -1,6 +1,7 @@
 import { configureStore, type Middleware } from "@reduxjs/toolkit";
 
-import usersReducer from "./users/slice";
+import { toast } from "sonner";
+import usersReducer, { rollbackUser, type UserId } from "./users/slice";
 
 const persistanceLocalStorageMiddleware: Middleware =
 	(store) => (next) => (action) => {
@@ -8,11 +9,44 @@ const persistanceLocalStorageMiddleware: Middleware =
 		localStorage.setItem("__redux__state__", JSON.stringify(store.getState()));
 	};
 
+const syncWithDatabaseMiddleware: Middleware =
+	(store) => (next) => (action) => {
+		const { type, payload } = action;
+		const previousState = store.getState();
+
+		next(action);
+
+		if (type === "users/deleteUserById") {
+			const userIdToRemove = payload;
+			const userToRemove = previousState.users.find(
+				(u: { id: UserId }) => u.id === userIdToRemove,
+			);
+
+			fetch(`https://jsonplaceholder.typicode.com/users/${userIdToRemove}`, {
+				method: "DELETE",
+			})
+				.then((res) => {
+					if (!res.ok) {
+						throw new Error("Something went wrong");
+					}
+					toast.success("User deleted successfully");
+				})
+				.catch(() => {
+					toast.error(
+						"There was an error while deleting the user. Please try again later.",
+					);
+					if (userToRemove) {
+						store.dispatch(rollbackUser(userToRemove));
+					}
+				});
+		}
+	};
+
 export const store = configureStore({
 	reducer: {
 		users: usersReducer,
 	},
-	middleware: [persistanceLocalStorageMiddleware],
+	middleware: [persistanceLocalStorageMiddleware, syncWithDatabaseMiddleware],
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
--- projects/crud-react-redux-toolkit/src/store/users/slice.ts
+++ projects/crud-react-redux-toolkit/src/store/users/slice.ts
@@ -33,7 +33,7 @@ const DEFAULT_STATE: UserWithId[] = [
 	},
 ];
 
-// Self-invoking function to get the initial state from localStorage
+// self-invoking function to get the initial state from localStorage
 const initialState: UserWithId[] = (() => {
 	const persistedState = localStorage.getItem("__redux__state__");
 	return persistedState ? JSON.parse(persistedState).users : DEFAULT_STATE;
@@ -45,22 +45,32 @@ export const usersSlice = createSlice({
 	reducers: {
 		addNewUser: (state, action: PayloadAction<User>) => {
 			const id = crypto.randomUUID().slice(0, 8);
-			const user = { ...action.payload, id };
+			const user = { id, ...action.payload };
 			state.push(user);
 		},
 		updateUserById: (state, action: PayloadAction<UserWithId>) => {
 			const user = action.payload;
 			const index = state.findIndex((u) => u.id === user.id);
-			state[index] = user;
+			if (index >= 0) {
+				state[index] = user;
+			}
 		},
 		deleteUserById: (state, action: PayloadAction<UserId>) => {
 			const id = action.payload;
 			return state.filter((user) => user.id !== id);
 		},
+		rollbackUser: (state, action: PayloadAction<UserWithId>) => {
+			const isUserAlreadyDefined = state.some(
+				(user) => user.id === action.payload.id,
+			);
+			if (!isUserAlreadyDefined) {
+				state.push(action.payload);
+			}
+		},
 	},
 });
 
-export const { addNewUser, updateUserById, deleteUserById } =
+export const { addNewUser, updateUserById, deleteUserById, rollbackUser } =
 	usersSlice.actions;
 
 export default usersSlice.reducer;
